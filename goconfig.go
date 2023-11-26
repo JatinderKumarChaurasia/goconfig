542d56f2a6a1731e238a8891f6b00e68d3964370
Jatinder Kumar Chaurasia <jatinderkumarchaurasia@gmail.com>
2023-11-26 21:04:48 +0530

    updating go.sum

--- goconfig.go
+++ goconfig.go
@@ -1,15 +1,5 @@
 package goconfig
 
-import "log"
-
 import (
 	_ "github.com/jatinderkumarchaurasia/goconfig/filelooker"
 )
-
-func init() {
-	println("setting go configurations")
-}
-
-func GoConfig() {
-	log.Println("hello world")
-}
